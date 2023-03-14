8680c020f264abccbf777cb7d8152525e1accfe3
vijayyagik1 <lvyagik@gmail.com>
2023-03-15 04:15:22 +0530

    react_day6_assignment

--- src/App.js
+++ src/App.js
@@ -1,28 +1,80 @@
-// import logo from './logo.svg';
+import React, { useRef, useState } from "react";
+import "./App.css";
+import { MdCancel } from "react-icons/md";
 
-import './App.css';
-// import Component1 from './Component1'
-// import Component2 from './Component2'
+const App = () => {
+  const [text, setText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const initialValue = useRef(0);
 
-function App() {
+  const handelChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handelAdding = () => {
+    const newUser = {
+      id: new Date() + text,
+      title: text,
+      status: false,
+    };
+    setTodos([...todos, newUser]);
+    setText("");
+  };
+
+  const handelDelete = (id) => {
+    const filterData = todos.filter((todo) => todo.id != id);
+    setTodos(filterData);
+  };
+
+  const handelStatus = (id) => {
+    const updateData = todos.map((item) => {
+      if (item.id === id) {
+        initialValue.current = initialValue.current + 1;
+        return { ...item, status: !item.status };
+      } else {
+        return item;
+      }
+    });
+    setTodos(updateData);
+  };
+
   return (
-    <div className="main">
-      <div className="App">
-        <h1><b>Register here...</b></h1>
-        <input type="text" placeholder="mobile number..."></input>
-        <br>
-        </br>
-        <br></br>
-        <input type="text" placeholder="password..."></input>
-        <br>
-        </br>
-        <br></br>
-        <button>Submit</button>
-        {/* <Component1/> */}
-        {/* <Component2/>   */}
+    <div className="container">
+      <h1>Pending Task {todos.length - initialValue.current}</h1>
+      <div>
+        {todos &&
+          todos.map((ele, index) => (
+            <div key={index} className="Single-data">
+              {ele.status ? (
+                <p>
+                  {" "}
+                  <strike> {ele.title}</strike>
+                </p>
+              ) : (
+                <p>{ele.title}</p>
+              )}
+              <div>
+                <button onClick={() => handelStatus(ele.id)}>Completed</button>
+                <MdCancel
+                  className="icon"
+                  onClick={() => handelDelete(ele.id)}
+                />
+              </div>
+            </div>
+          ))}
+      </div>
+      <div className="input-part">
+        <input type="text" value={text} onChange={(e) => handelChange(e)}/>
+        <button onClick={handelAdding}>Add</button>
+        
       </div>
     </div>
   );
-}
+};
+
 
 export default App;
+
+
+
+
--- src/component1.js
+++
@@ -1,6 +0,0 @@
-const First = function (){
-    return (
-        <h1><b>Register here...</b></h1>
-    )
-};
-export default First;
