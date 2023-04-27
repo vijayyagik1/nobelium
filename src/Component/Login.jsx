c4187e73fad24c90b97ce7143d3ce6c95e5576e5
shivamdaksh404 <shivofficial05@gmail.com>
2023-04-27 12:02:21 +0530

    login page local storage update

--- src/Component/Login.jsx
+++ src/Component/Login.jsx
@@ -1,57 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import { isUserLoggedIn } from "../Data/AtomData/Atom";
 import { useRecoilState } from "recoil";
 import { useNavigate } from "react-router-dom";
-import { Button, TextField  } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 import style from "./Login.module.css";
-import {FaTwitter} from 'react-icons/fa';
+import { FaTwitter } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 import { BsApple } from "react-icons/bs";
 
 const Login = () => {
   const [isLogIn, setIsLogIn] = useRecoilState(isUserLoggedIn);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const navigate = useNavigate();
+
+  const userData = [];
+  // const userData = localStorage.getItem(JSON.parse('user'))
+  const handleLogin = () => {
+    const currentUser = userData.find((data) => data.email === email);
+    if (currentUser.password === password) {
+      alert("Login successful");
+      navigate("/");
+    } else if (!currentUser) {
+      alert("User Not Found");
+    }
+  };
+
+  console.log(email, password);
 
   return (
     <div className={style.mainContainer}>
-    <div className={style.heading}>
-<FaTwitter/>
-    <h1>Sign in to Twitter</h1>
-    </div>
+      <div className={style.heading}>
+        <FaTwitter />
+        <h1>Sign in to Twitter</h1>
+      </div>
       <div className={style.apple}>
         <Button variant="outlined" startIcon={<FcGoogle />}>
           Sign in with Google
-        </Button >
-        <Button variant="outlined"   startIcon={<BsApple />}>
-          Sign in with Apple 
+        </Button>
+        <Button variant="outlined" startIcon={<BsApple />}>
+          Sign in with Apple
         </Button>
       </div>
       <div className={style.sectionDivider}>
-       <div><hr/></div>
-        <span>or</span>
-       <div><hr/></div>
+        <div>
+          <hr />
+        </div>
+        <b>or</b>
+        <div>
+          <hr />
+        </div>
       </div>
 
       <form className={style.login}>
         <TextField
-          
           id="outlined-basic"
-          label="Phone, Email Or UserName"
+          label="Phone, email Or username"
           variant="outlined"
+          onChange={(e) => setEmail(e.target.value)}
         />
         <TextField
-          type='password'
+          type="password"
           id="outlined-basic"
           label="Password"
           variant="outlined"
+          onChange={(e) => setPassword(e.target.value)}
         />
 
-        <Button sx={{backgroundColor:'black', fontWeight:'bold'}} variant="contained">Log In </Button>
+        <Button
+          onClick={handleLogin}
+          sx={{ backgroundColor: "black", fontWeight: "bold" }}
+          variant="contained"
+        >
+          Log In{" "}
+        </Button>
         <Button variant="outlined">Forgot Password? </Button>
       </form>
       <div className={style.switch}>
         <span>Don't Have an Account?</span> &nbsp;
-        <span onClick={()=>navigate('/signup')}>Sign Up</span>
+        <span onClick={() => navigate("/signup")}>Sign Up</span>
       </div>
     </div>
   );
