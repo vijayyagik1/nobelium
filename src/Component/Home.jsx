1eec205206cb130d987b5ef1a1459ad04fa0c838
Uttu32 <utkarshksharm7878@gmail.com>
2023-05-01 04:35:33 +0530

    Registration update

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,5 +1,5 @@
 import LeftSideBar from "../HomeComponent/LeftSideBar";
-import React,{useState} from "react";
+import React, { useState } from "react";
 import RightSideBar from "../HomeComponent/RightSideBar";
 import { useRecoilState } from "recoil";
 import Styles from "./Home.module.css";
@@ -13,35 +13,30 @@ import HomeHeader from "../HomeComponent/HomeHeader";
 import TweetModel from "../HomeComponent/TweetModel";
 
 import { getCurrentUser } from "../services/utilities";
-import SearchBar from "../HomeComponent/SearchBar"
-
+import SearchBar from "../HomeComponent/SearchBar";
 
 const Home = () => {
   const [isLoggedIn, setIsLoggedIn] = useRecoilState(isUserLoggedIn);
   const navigate = useNavigate();
 
-
   useEffect(() => {
-    const currentUser = getCurrentUser()
-    setIsLoggedIn(currentUser.isLoggedIn)
-    console.log(isLoggedIn)
+    const currentUser = getCurrentUser();
+    setIsLoggedIn(currentUser.isLoggedIn);
+    console.log(isLoggedIn);
     if (isLoggedIn === true) {
       fetch("/data.json")
-      .then((res) => res.json())
-      .then((data) => localStorage.setItem("posts", JSON.stringify(data)))
+        .then((res) => res.json())
+        .then((data) => localStorage.setItem("posts", JSON.stringify(data)));
+    } else {
+      navigate("/signIn");
     }
-    else {
-      navigate("/signIn")
-    }
-  },[isLoggedIn]);
-
-
+  }, [isLoggedIn]);
 
   return (
     <div className={Styles.container}>
       <div className={Styles.leftDiv}>
         <LeftSideBar />
-        <TweetModel/>
+        <TweetModel />
       </div>
       <div className={Styles.centerDiv}>
         <HomeHeader />
@@ -49,13 +44,16 @@ const Home = () => {
 
         <MainComponent />
       </div>
-      <div  className={Styles.RMain}>
-      <div><SearchBar/></div>
-      <div className={Styles.RightMain}>
-        <div className={Styles.Content}>
-          <RightSideBar/>
+      <div className={Styles.RMain}>
+        <div>
+          <SearchBar />
         </div>
-      </div></div>
+        <div className={Styles.RightMain}>
+          <div className={Styles.Content}>
+            <RightSideBar />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
