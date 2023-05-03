d711d741d07963055eb32a9cc5800ba222df56d4
vijayyagik1 <126654908+vijayyagik1@users.noreply.github.com>
2023-05-03 22:41:12 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+// import './style.css';
+import Select from 'react-select';
+export default function App() {
+  const [data, setData] = useState(null);
+  const onption = [
+    { value: 'apple', label: 'apple' },
+    { value: 'banana', label: 'banana' },
+    { value: 'mango', label: 'mango' },
+  ];
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Select options={onption} value={data} onChange={setData} isSearchable />
     </div>
   );
 }
-
-export default App;
