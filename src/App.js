5ae2ee2c495b8308abab4e3ed09ef32676d467ad
vijayyagik1 <126654908+vijayyagik1@users.noreply.github.com>
2023-05-03 23:19:37 +0530

    calc

--- src/App.js
+++ src/App.js
@@ -1,17 +1,60 @@
-import React, { useState } from 'react';
-// import './style.css';
-import Select from 'react-select';
-export default function App() {
-  const [data, setData] = useState(null);
-  const onption = [
-    { value: 'apple', label: 'apple' },
-    { value: 'banana', label: 'banana' },
-    { value: 'mango', label: 'mango' },
-  ];
+import React, { useState } from "react";
+import "./App.css";
 
+export default function App() {
+  let numbers = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "+",
+    "-",
+    "*",
+    "/",
+    "%",
+    ".",
+  ];
+  // let operators=[,]
+  const [value, setValue] = useState("");
+  const [count, setcount] = useState(0);
+  function handleClick(id) {
+    if (value === "0") {
+      setValue(numbers[id]);
+    } else {
+      if (count === 1) {
+        setcount(0);
+        setValue(numbers[id]);
+        return;
+      }
+      setValue(value + numbers[id]);
+    }
+  }
+  function handleres() {
+    const result = eval(value);
+    setValue(result);
+    setcount(count + 1);
+  }
+  function handleClear() {
+    setValue("");
+  }
   return (
-    <div>
-      <Select options={onption} value={data} onChange={setData} isSearchable />
+    <div className="container">
+      <input value={value} />
+      <div className="numbers">
+        {numbers.map((num, index) => (
+          <button key={index} onClick={(e) => handleClick(index)}>
+            {num}
+          </button>
+        ))}
+        <button onClick={handleres}>=</button>
+        <button onClick={handleClear}>clear</button>
+      </div>
     </div>
   );
 }
